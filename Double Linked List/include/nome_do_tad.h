466c6b676f7cdd96fe2c1bcae099820c615a0260
luckera <lucasromero1702@gmail.com>
2022-09-26 17:17:39 -0300

    Creation of  Double Linked List File

---
+++ Double Linked List/include/nome_do_tad.h
@@ -0,0 +1,14 @@
+#ifndef NOME_DO_TAD_H
+#define NOME_DO_TAD_H
+
+//*************************** HEADER DO TAD - ESPECIFICAÇÃO ***************************//
+
+typedef struct ESTRUTURA ESTRUTURA_APELIDO;
+
+// Assinatura das Funções do TAD
+
+
+
+//************************************************************************//
+
+#endif
---
+++ Double Linked List/src/nome_do_tad.c
@@ -0,0 +1,6 @@
+#include "nome_do_tad.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//*************************** IMPLEMENTAÇÃO DO TAD ***************************//
