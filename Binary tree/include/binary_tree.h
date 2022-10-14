ae8d2d476ca0c8b056b81f0350ec56ce927cb872
Lucas Lima Romero <101420277+luckera@users.noreply.github.com>
2022-10-14 12:48:09 -0300

    Alteração em Árvore Binária

--- Binary tree/include/binary_tree.h
+++ Binary tree/include/binary_tree.h
@@ -1,3 +1,13 @@
-typedef struct No *ArvBin; // Criação do ponteiro para a estrutura raiz
+typedef struct _node Node; // Criação do ponteiro para a estrutura raiz
+typedef struct _item Item;
 
 // Função para criação da árvore - Cria a raiz da árvore
+ArvBin* ArvBin_create();
+
+// Funções para desalocar a memória da Árvore
+// . Liberá nós . Libera raiz
+void ArvBin_destroy_node(Arv);
+void ArvBin_destroy_root();
+
+
+ 
--- Binary tree/src/binary_tree.c
+++ Binary tree/src/binary_tree.c
@@ -2,9 +2,28 @@
 #include <stdlib.h>
 #include "binary_tree.h"
 
-typedef struct _no{
-    int info;
-    struct _no *esq;
-    struct _no *dir; 
-} No;
+typedef struct _item{
+    int cod;
+} Item;
+
+typedef struct _node{
+    Item item;
+    struct _node *left;
+    struct _node *right;
+} Node;
+
+// Criação de um nó especial raiz que aponta para o início da árvore
+
+ArvBin *ArvBin_create(){
+    // Criação de um ponteiro para o tipo ArvBin
+    // ArvBin é um ponteiro que aponta para nós
+    // Logo, raiz é um ponteiro de ponteiro
+    ArvBin *root = (ArvBin*) calloc(1, sizeof(ArvBin));
+    if(root != NULL){
+        *root = NULL;
+    }
+    
+
+    return root;
+}
 
