eb40c735f37cb3eebb47be9676e1f5c13384dabe
luckera <lucasromero1702@gmail.com>
2022-10-15 15:27:38 -0300

    Vrum vrum

--- Binary tree/BinaryTree1/include/binary_tree.h
+++ Binary tree/BinaryTree1/include/binary_tree.h
@@ -1,7 +1,12 @@
+#ifndef NOME_DO_TAD_H
+#define NOME_DO_TAD_H
 
-
+//*************************** HEADER DO TAD - ESPECIFICAÇÃO ***************************//
 
 typedef struct _node *ArvBin; // Criação do ponteiro para a estrutura raiz
+
+
+// Assinatura das Funções do TAD
 
 // Função para criação da árvore - Cria a raiz da árvore
 ArvBin *ArvBin_create();
@@ -9,6 +14,8 @@ ArvBin *ArvBin_create();
 // Funções para desalocar a memória da Árvore
 // . Liberá nós . Libera raiz
 
+//************************************************************************//
 
+#endif
 
  
